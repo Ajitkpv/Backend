6faebff51c6679ae8a21d50beada0bb83c1b6a90
ajitkp <ajitkp18@gmail.com>
2024-01-04 23:13:17 +0530

    first commit By Ajit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/firstProgram', (req, res) => {
+    res.send('First Program Run')
+})
+  
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
